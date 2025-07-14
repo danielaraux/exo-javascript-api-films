ca477592e000f2e8cd933f3b43505fda37ca951f
daniel <danielaraux@outlook.com>
2025-07-14 18:56:40 +0200

    id et recherche

--- assets/js/description.js
+++ assets/js/description.js
@@ -1,5 +1,3 @@
-let apiKey = 'bde750cc5b249c3a5ee817159a7cd5e8'
-
 const options = {
     method: 'GET',
     headers: {
@@ -8,10 +6,72 @@ const options = {
     }
 };
 
-fetch('https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1', options)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
+
+// const parsedUrl = new URL(window.location.href);
+
+// let id = parsedUrl.searchParams.get("id")
+
+let id = 1374534
 
 
+fetch(`https://api.themoviedb.org/3/movie/${id}?language=fr-FR`, options)
+    .then(response => response.json())
+    .then(insideFilm => {
+        console.log(insideFilm)
+
+        console.log(insideFilm.genres[0].name)
+
+        // actuellement on est dans data.result[1] dans l'arbo du film directement
+
+
+        // for (i = 0; i < insideFilm.genres.length; i++) {
+        //     console.log(insideFilm.genres[i].name)
+        //     let d = insideFilm.genres[i].name
+        //     console.log(d)
+        // }
+
+
+
+        // Affiche du film
+        document.getElementById("divcards").innerHTML += `
+        <div><img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${insideFilm.poster_path}" alt="affiche du film" style="width:20rem" class="my-4 mx-5"></div>
+            <div class="px-4 my-4">
+                <h1><b>${insideFilm.title}</b></h1>
+                <div><span>Date de sortie : ${insideFilm.release_date}</span> • <span>Genre : ${insideFilm.genres[0].name}</span> • <span>Durée : ${insideFilm.runtime} min</span></div>
+                <div>Note : ${insideFilm.vote_average}/10</div>
+                <h4 class="my-3">Synopsis :</h4>
+                <div>${insideFilm.overview}</div>
+            </div>
+        `
     })
+    .catch(err => console.error(err));
+
+
+
+//  Cards des acteurs
+
+fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=fr-fr`, options)
+    .then(response => response.json())
+    .then(actors => {
+        console.log(actors)
+
+        console.log(actors.cast[0].character)
+
+
+        for (let i = 0; i < actors.cast.length; i++) {
+
+            document.getElementById("actors").innerHTML +=
+                `
+                <div class="card m-2" style="width: 10rem;">
+                <img src="https://image.tmdb.org/t/p/w500${actors.cast[i].profile_path}" class="card-img-top" alt="actors profile picture">
+                
+                <div class="card-body">
+                    <h5 class="card-title"><b>${actors.cast[i].name}</b></h5>
+                    <p class="card-text">${actors.cast[i].character}</p >
+                </div >
+            </div >
+                `
+
+        }
+    })
+    .catch(err => console.error(err));
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-let apiKey = 'bde750cc5b249c3a5ee817159a7cd5e8'
-
 const options = {
     method: 'GET',
     headers: {
@@ -10,6 +8,9 @@ const options = {
 
 // 
 
+
+
+
 fetch('https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1', options)
     .then(res => res.json())
     .then(data => {
@@ -19,63 +20,25 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1', op
         console.log(data.results[1].title)
 
 
-
         // Boucle for pour créer les cards
         for (let i = 0; i <= 19; i++) {
+            console.log(data.results[i].id)
             document.getElementById("container-card").innerHTML +=
                 `
-           <a href="description.html" class="text-decoration-none">
-           <div class="card"
-                <div class="card" style="width: auto;">
-                        <div>
-                            <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.results[i].poster_path}" class="card-img-top" alt="affiche du film
-                        </div>
+        <a href="description.html" class="text-decoration-none">
+        <div class="card"
+             <div class="card" style="width: 18rem;">
+                     <div>
+                         <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.results[i].poster_path}" style="width:18rem"  alt="affiche du film
+                     </div>
         
-                    <div class="card-body">
-                        <h5 class="card-title"><b>${data.results[i].title}</b></h5>
-                        <p class="card-text"><div>Date de sortie : ${data.results[i].release_date}</p>
-                        <p class="card-text">Note moyenne : ${data.results[i].vote_average}</p>
-                    </div>
-                </div>
-            </div></a>
-            `
+                 <div class="card-body">
+                     <h5 class="card-title m-2"><b>${data.results[i].title}</b></h5>
+                     <p class="card-text m-2">Date de sortie : ${data.results[i].release_date}</p>
+                     <p class="card-text m-2">Note moyenne : ${data.results[i].vote_average}</p>
+                 </div>
+             </div>
+         </div></a>
+         `
         }
     })
-
-
-
-
-// Solution possible pour la description du film :
-// Créer un autre fichier.js, ajouter la variable parse qu'on a mise en haut de notre fichier actuel
-// const parsedUrl = new URL(window.location.href);
-// console.log(parsedUrl.searchParams.get("id")); // "123"
-
-// Mettre l'id dans l'url du fetch de l'autre fichier
-
-// let id = parsedUrl.searchParams.get("id")
-
-
-
-
-
-// onclick pour le apiKey, le i qui changera le film
-
-
-
-
-
-
-
-
-// .catch(err => console.error(err))
-
-
-// function showDetails(data) {
-//     console.log(data)
-
-// }
-
-
-
-// poster de ballerina : "/2VUmvqsHb6cEtdfscEA6fqqVzLg.jpg"
-// id du film ballerina : 541671
